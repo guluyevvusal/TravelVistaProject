d2e03be835435e2629e9077a6f56d0577d21e32a
vüsal <vsal@LAPTOP-8PPT5PTN>
2024-08-27 17:15:30 +0400

    new Entities created

--- EntityLayer/EntityLayer/Concrete/Feature.cs
+++ EntityLayer/EntityLayer/Concrete/Feature.cs
@@ -6,22 +6,12 @@ using System.Threading.Tasks;
 
 namespace EntityLayer.Concrete
 {
-    public class About
+    public class Feature
     {
-        public  int AboutId { get; set; }
-
+        public  int FeatureId { get; set; }
         public  string Title { get; set; }
-
         public  string Description { get; set; }
-
-        public  string Image1 { get; set; }
-
-        public  string Title2 { get; set; }
-
-        public  string Description2 { get; set; }
-
-        public bool  Status { get; set; }
-
-
+        public  string Image { get; set; }
+        public  bool Status { get; set; }
     }
 }
