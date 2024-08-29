ba58ce05a99d9d12d51678e78fedfb81c856a368
vüsal <vsal@LAPTOP-8PPT5PTN>
2024-08-29 13:35:11 +0400

    AboutManager created

---
+++ BusinessLayer/BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        IAboutDAL _aboutdal;
+
+        public AboutManager(IAboutDAL aboutDAL)
+        {
+            _aboutdal = aboutDAL;
+
+        }
+
+        public About TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TAdd(About t)
+        {
+            _aboutdal.Insert(t);
+        }
+
+        public void TDelete(About t)
+        {
+            _aboutdal.Delete(t);
+        }
+
+        public List<About> TGetList()
+        {
+           return _aboutdal.GetList();
+        }
+
+        public void TUpdate(About t)
+        {
+           _aboutdal.Update(t);
+        }
+    }
+}
